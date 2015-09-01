5a2349cc68efa400ad54cfc9d4e7c783c57da8e4
mrsbuddhaman <mrs.buddhaman@gmail.com>
2015-09-01 19:23:14 -0400

    add test for first element in array having value 1. update greene_county function

--- GC_kata.rb
+++ GC_kata.rb
@@ -1,3 +1,3 @@
 def greene_county
-	Array.new(100,"anything")
+	Array.new(100, 1)
 end
--- GC_kata_test.rb
+++ GC_kata_test.rb
@@ -4,9 +4,14 @@ require "minitest/autorun"
 class GC_kata_test < Minitest::Test
 
 	def test_create_array_with_100_elements
-		array = greene_county
-		assert_equal(100, array.length)
+		results = greene_county
+		assert_equal(100, results.length)
 	end
-
-
+	def test_element_in_array_is_1
+		results = greene_county
+		assert_equal(1, results[0])
+	end
+	
+	
+	
 end
