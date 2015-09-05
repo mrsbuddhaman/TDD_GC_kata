f9ab93e277a4b6a6ff492f6dcb58ea3331e54c27
mrsbuddhaman <mrs.buddhaman@gmail.com>
2015-09-04 20:55:11 -0400

    added test to check if value of element is mod 3 and mod 5

--- GC_kata.rb
+++ GC_kata.rb
@@ -1,9 +1,15 @@
 def greene_county
 	our_array = [*(1..100)]	
 	our_array.each.with_index do |value, index_position| 
-		if value % 3 == 0
-			our_array[index_position] = "greene"		
+		if value % 3 == 0 && value % 5 == 0
+			our_array[index_position] = "greene county"
 		end
+		
+		if value % 3 == 0
+			our_array[index_position] = "greene"
+			
+		end
+		
 		if value % 5 == 0
 			our_array[index_position] = "county"
 		end
