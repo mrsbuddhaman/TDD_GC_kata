ff1d3de3ba74aa75d3dfc20dc2d8145199c17ab8
mrsbuddhaman <mrs.buddhaman@gmail.com>
2015-09-16 21:54:13 -0400

    added test for 68 cents

--- coin_changer_test.rb
+++ coin_changer_test.rb
@@ -6,29 +6,36 @@ class Coin_Changer_test < Minitest::Test
 	def test_for_one_cent
 		assert_equal({penny: 1, nickel: 0, dime: 0, quarter: 0}, coin_changer(1))
 	end
+	
 		
 	def test_for_five_cents
 		assert_equal({penny: 0, nickel: 1, dime: 0, quarter: 0}, coin_changer(5))
 	end
 	
+	
 	def test_for_ten_cents
 		assert_equal({penny: 0, nickel: 0, dime: 1, quarter: 0}, coin_changer(10))
 	end
+	
 	
 	def test_for_25_cents
 		assert_equal({penny: 0, nickel: 0, dime: 0, quarter: 1}, coin_changer(25))
 	end
 	
+	
 	def test_for_six_cents
 		assert_equal({penny: 1, nickel: 1, dime: 0, quarter: 0}, coin_changer(6))
 	end
 	
+	
 	def test_for_2_cents
 		assert_equal({penny: 2, nickel: 0, dime: 0, quarter: 0}, coin_changer(2))
 	end
-
-
-
+	
+	
+	def test_for_68_cents
+		assert_equal({penny: 3, nickel: 1, dime: 1, quarter: 2}, coin_changer(68))
+	end
 
 
 
